09e20e90b9cdbb9c1a7b3669e35a2004280c9577
Vivek Nemade <101920079+VivekN11@users.noreply.github.com>
2022-05-26 22:51:47 +0530

    updated 1

--- src/App.js
+++ src/App.js
@@ -6,16 +6,37 @@ function App() {
 
   const [page, setPage] = useState(1) 
   const [limit, setLimit] = useState(5)
+  const [newTodo, setNewTodo] = useState("")
   const [totalCount, setTotalCount] = useState(0) 
   const [todos, setTodos] = useState([]) 
   
+  const SaveTodo=()=>{
+    fetch("http://localhost:8080/todos",{
+        method: "POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify({
+        value: newTodo,
+        isCompleted: false,
+    }),
+})
+    .then((r)=> r.json())
+    .then((d)=>{
+        setTodos([...todos,d])
+        setNewTodo("")
+    });
+
+};
+
+
 
   useEffect(()=>{
     axios.get(`http://localhost:8080/todos?_page=${page}&_limit=${limit}`).then((r)=>{
       setTodos(r.data);
       setTotalCount(Number(r.headers["x-total-count"]));
     })
-  },[page,limit])
+  },[page,limit,todos])
 
 
 
@@ -29,6 +50,11 @@ function App() {
                   <option value={15} >15</option>
                 </select>
               <button disabled={totalCount < page * 5} onClick={()=>setPage(page+1)}>Next</button>
+
+              <div>
+                <input value={newTodo} onChange={({target})=>setNewTodo(target.value)}/>
+                <button onClick={SaveTodo}>+</button>
+            </div>
 
 
           {todos.map((todo)=>(
