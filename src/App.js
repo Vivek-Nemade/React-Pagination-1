0c23197c5ba4f2b86f7bbce06b933c38f57728bc
Vivek Nemade <101920079+VivekN11@users.noreply.github.com>
2022-05-26 22:40:25 +0530

    pagination first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [page, setPage] = useState(1) 
+  const [limit, setLimit] = useState(5)
+  const [totalCount, setTotalCount] = useState(0) 
+  const [todos, setTodos] = useState([]) 
+  
+
+  useEffect(()=>{
+    axios.get(`http://localhost:8080/todos?_page=${page}&_limit=${limit}`).then((r)=>{
+      setTodos(r.data);
+      setTotalCount(Number(r.headers["x-total-count"]));
+    })
+  },[page,limit])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <button disabled={page<=1} onClick={()=>setPage(page-1)}>Previous</button>
+                <select onChange={(e)=>setLimit(Number(e.target.value))}>
+                  <option value={5} >5</option>
+                  <option value={10} >10</option>
+                  <option value={15} >15</option>
+                </select>
+              <button disabled={totalCount < page * 5} onClick={()=>setPage(page+1)}>Next</button>
+
+
+          {todos.map((todo)=>(
+            <div key={todo.id}>{todo.id}{` : `} {todo.value}</div>
+            
+          ))}
+          
+          
     </div>
   );
 }
